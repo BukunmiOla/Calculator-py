40612c52a79ede8e099aedf7456d4e71e2eac889
BukunmiOla <ebukunmiola@gmail.com>
2020-03-25 00:01:03 +0100

    area

---
+++ Calculator.py
@@ -0,0 +1,6 @@
+import math
+
+print ("Hi! Let's calculate the area of a cirlce")
+r = int(input("Enter the radius here: "))
+area = math.pi * r * r
+print ("Yo! The area of that circle is " , round (area,2))
